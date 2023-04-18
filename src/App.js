157eefb36afb67890c07bcde7c9e0c5e5302e2d8
IlanVaks <ilanvaks@gmail.com>
2023-04-18 19:50:23 -0400

    added-Portfolio-Footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-
-import './App.css';
+import Footer from './components/Footer';
 import AboutMe from './components/Portfolio';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <AboutMe/>
+        <Footer/>
         
       </header>
     </div>
---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+export default function Footer () {
+
+  
+  return (
+    <>
+    <footer>
+      <p><small>&copy; Ilan Vaks<br />
+      <a href="https://github.com/ilanvaks" rel="noreferrer" target="_blank">Github</a></small></p>
+    </footer>
+    
+    </>
+  )
+}
